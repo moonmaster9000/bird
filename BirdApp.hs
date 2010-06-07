5f5cba2aa262b9f8359951f7a252cdf56eeec7a0
Matt Parker <mparker@ml-ny-mparker.local>
2010-06-07 16:19:58 -0400

    get method now of return type IO monad
      -- now you can actually do IO to generate your response

--- BirdApp.hs
+++ BirdApp.hs
@@ -2,16 +2,16 @@ module BirdApp where
 import Bird
 import Data.String.Utils
 
-get :: Request -> Reply
+get :: Request -> IO Reply
 
 get Request { path = [] }           
-  = ok { bod = "Hello, World!" }
+  = return $ ok { bod = "Hello, World!" }
 
 get Request { path = ("hello":xs) } 
-  = ok { bod = (++) "Hello " $ join ", " xs }
+  = return $ ok { bod = (++) "Hello " $ join ", " xs }
 
 get Request { path = ("echo":w:[]) }
-  | length w == 4 = forbidden { bod = "We cannot echo 4 letter words." }
-  | otherwise     = ok { bod = w }
+  | length w == 4 = return $ forbidden { bod = "We cannot echo 4 letter words." }
+  | otherwise     = return $ ok { bod = w }
 
-get _ = notFound
+get _ = return $ notFound
--- Main.hs
+++ Main.hs
@@ -4,15 +4,16 @@ import Bird
 import BirdApp
 
 app :: Application
-app = \e -> return $ route e
+app = \e -> route e
 
-route :: Env -> Response
+route :: Env -> IO Response
 route e = response
   where 
     r = envToRequest e
-    response = replyToResponse $ 
-      case verb r of 
-        GET -> get r
-        _   -> error "not supported"
-
+    response = do 
+      r <-  
+        case verb r of 
+          GET -> get r
+          _   -> error "not supported"
+      return $ replyToResponse r
 main = run app

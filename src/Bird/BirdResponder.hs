27948dbb34507b81331c1adb959fc0262505ac72
Jason Dew <jason.dew@gmail.com>
2010-08-08 08:55:01 +0800

    adding rudimentary logging

--- src/Bird/BirdResponder.hs
+++ src/Bird/BirdResponder.hs
@@ -7,11 +7,19 @@ import Data.Maybe
 import Bird.Reply
 import Bird.Request
 import qualified Data.Map as Hash
+import Text.Printf
+import System.CPUTime
 
 type BirdResponder = StateT Reply (ReaderT Request IO)
 
-runBirdResponder request router = 
-  runReaderT (execStateT (router request) def) request
+runBirdResponder request router = do
+  printf "Processing %s request of %s with %s\n" (show $ verb request) (show $ path request) (show $ params request)
+  start <- getCPUTime
+  reply <- runReaderT (execStateT (router request) def) request
+  end   <- getCPUTime
+  printf "  Response code: %s\n" (show $ replyStatus reply)
+  printf "  Response time: %0.3fs\n\n" (((fromIntegral (end - start)) / (10^12)) :: Double)
+  return reply
 
 body b = do
   reply <- get
